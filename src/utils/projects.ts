171fd8cd3cf7eb2dd4beb5e4b76dc1a561cfc1f8
Thomas Ejembi <ejembithomas61@gmail.com>
2025-02-12 20:34:14 +0100

    updates on the image link

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -2,7 +2,7 @@ export const projects = {
 	web: [
 		{
 			id: 1,
-			imgSrc: "./src/image/Krext.webp",
+			imgSrc: "../src/image/Krext.webp",
 			header: "Krext",
 			description: "Simplify the Process of Planning and Executing Meetings",
 			link: "https://krext.com/",
@@ -17,7 +17,7 @@ export const projects = {
 		},
 		{
 			id: 2,
-			imgSrc: "./src/Image/LearnAfrica.webp",
+			imgSrc: "../src/Image/LearnAfrica.webp",
 			header: "LearnAfrica",
 			description:
 				"Access thousands of school textbooks and learning materials online.",
@@ -32,7 +32,7 @@ export const projects = {
 		},
 		{
 			id: 3,
-			imgSrc: "./src/Image/KOI.webp",
+			imgSrc: "../src/Image/KOI.webp",
 			header: "KOI",
 			description:
 				"Pioneering a New Era of Profit Distribution in Social Networks for Greater Transparency and Fairness.",
@@ -41,7 +41,7 @@ export const projects = {
 		},
 		{
 			id: 4,
-			imgSrc: "./src/Image/Decove.webp",
+			imgSrc: "../src/Image/Decove.webp",
 			header: "Decove",
 			description: "Fresh Grocery Delivered To Your Door Step.",
 			link: "https://decove.com/",
@@ -49,7 +49,7 @@ export const projects = {
 		},
 		{
 			id: 5,
-			imgSrc: "./src/Image/Sky.webp",
+			imgSrc: "../src/Image/Sky.webp",
 			header: "Sky Venture",
 			description: "One world, one ecosystem #1 incubator",
 			link: "https://www.skyventure.io/",
@@ -59,7 +59,7 @@ export const projects = {
 	mobile: [
 		{
 			id: 1,
-			imgSrc: "./src/Image/Dryve.webp",
+			imgSrc: "../src/Image/Dryve.webp",
 			title: "Dryve",
 			description:
 				"Dryve app is a premium e-hailing travel experience. Sustainable rides in very executive vehicles for friendly trips. Just tap that button to order a ride, get picked up by a nearby driver, and enjoy a low-cost ride to your destination.",
@@ -74,7 +74,7 @@ export const projects = {
 		},
 		{
 			id: 2,
-			imgSrc: "./src/Image/Dryve.webp",
+			imgSrc: "../src/Image/Dryve.webp",
 			title: "Dryve Driver",
 			description:
 				"At Dryve, we believe that your journey should be as enjoyable as your destination. Whether you're commuting to work, heading out for a night on the town, or exploring the vibrant streets of Nigeria, Dryve is here to make your ride seamless, safe, and stress-freer",
@@ -90,7 +90,7 @@ export const projects = {
 		},
 		{
 			id: 3,
-			imgSrc: "./src/Image/Decove App.webp",
+			imgSrc: "../src/Image/Decove App.webp",
 			title: "Décove",
 			description:
 				"Tired of lugging heavy bags home from the grocery store? Let us do the heavy lifting!",
